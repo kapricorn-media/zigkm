725acfbdc44e06201c0f3ddc2dae179403e5ab90
Jose Rico <jmrico01@gmail.com>
2026-05-09 20:06:17 -0400

    upg

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -2,49 +2,60 @@
 
 org=kapricorn-media
 repo=$1
-dir=$2
 service=${PWD##*/}
+timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 
-if [ "$repo" == "" ] || [ "$dir" == "" ]
+if [ "$repo" == "" ]
 then
-	echo "Required argument <repo> <local dir>"
+	echo "Required argument <repo>"
 	exit 1
 fi
 
-echo Upgrading latest artifact from repo $org/$repo into local dir $dir.
+# echo Upgrading latest artifact from repo $org/$repo into local dir $dir.
 
-echo Backing up $dir...
-if [ -d "./$dir-bak" ]
-then
-    if [ -d "./$dir-bak2" ]
-    then
-        rm -rf "./$dir-bak2"
-    fi
-    mv "./$dir-bak" "./$dir-bak2"
-fi
-mv "./$dir" "./$dir-bak"
+# echo Backing up $dir...
+# if [ -d "./$dir-bak" ]
+# then
+#     if [ -d "./$dir-bak2" ]
+#     then
+#         rm -rf "./$dir-bak2"
+#     fi
+#     mv "./$dir-bak" "./$dir-bak2"
+# fi
+# mv "./$dir" "./$dir-bak"
 
 echo Downloading latest release artifact from $org/$repo...
 curl -o latest.zip "https://ci.kapricornmedia.com/latest_artifact?org=$org&repo=$repo"
-sha256sum latest.zip
+# hash=$(sha256sum latest.zip)
+# sha256sum latest.zip
+set -- $(sha256sum latest.zip)
+hash=$1
 unzip -o latest.zip
-tar -xf output_archive.tar.gz
+
+dir=${timestamp}_${repo}_${hash}
+echo Extracting into $dir
+echo $timestamp
+echo $repo
+echo $hash
+echo $dir
+exit 1
+
+tar -xf output_archive.tar.gz -C $dir
 if [ -d "./$dir" ]
 then
     echo Download succeeded.
 else
     echo Download+extract failed, aborting...
-    mv "./$dir-bak" "./$dir"
     exit 1
 fi
 
-if [ -d "./$dir-bak/keys" ]
-then
-    echo Copying keys...
-    cp -r "./$dir-bak/keys" "./$dir/keys"
-fi
-if [ -d "./$dir-bak/state" ]
-then
-    echo Copying state...
-    cp -r "./$dir-bak/state" "./$dir/state"
-fi
+# if [ -d "./$dir-bak/keys" ]
+# then
+#     echo Copying keys...
+#     cp -r "./$dir-bak/keys" "./$dir/keys"
+# fi
+# if [ -d "./$dir-bak/state" ]
+# then
+#     echo Copying state...
+#     cp -r "./$dir-bak/state" "./$dir/state"
+# fi
